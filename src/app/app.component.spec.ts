37bf108eef08ad8cbd958755e36c3d645d0b3b81
Yashi Singh <yashisingh1563@gmail.com>
2024-11-08 11:51:00 +0530

    initial-commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'swiggy-web-clone' title`, () => {
+  it(`should have the 'swiggyclone' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('swiggy-web-clone');
+    expect(app.title).toEqual('swiggyclone');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, swiggy-web-clone');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, swiggyclone');
   });
 });
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { RestaurantInfoComponent } from './restaurant-info/restaurant-info.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent }, 
+  { path: 'restaurant-details/:id', component: RestaurantInfoComponent } 
+];
